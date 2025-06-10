070304415535b4d21c34835bbde024329ae43679
AskKenImani <imamikenny27@gmail.com>
2025-06-10 18:16:46 +0100

    vite error

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-<<<<<<< HEAD
-=======
 // https://vitejs.dev/config/
->>>>>>> 04cedc4b9a115c572a13c7237d724817895cc315
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
